c48ebdd79ffbcca7ad46f2525314c374e9f2c638
Jayabharath1607 <116068745+Jayabharath1607@users.noreply.github.com>
2022-10-19 13:54:19 +0530

    Add files via upload

---
+++ areaofsquare.java
@@ -0,0 +1,12 @@
+
+public class areaofsquare {
+	public static void main(String[] args) {
+		float length, height,area;
+		length=67.3f;
+		height=12.3f;
+		
+		area=length*height;
+		System.out.println("Area of reactangle of length="+length+" height="+height +" is "+area);
+				
+}
+}
