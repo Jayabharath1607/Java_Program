8ef44b56c42dedfc98abfec191aa2511d9c6d199
Jayabharath1607 <116068745+Jayabharath1607@users.noreply.github.com>
2022-10-19 14:08:22 +0530

    Add files via upload

---
+++ areaofrect.java
@@ -0,0 +1,11 @@
+
+public class areaofrect {
+		public static void main(String args[])
+		{
+		int length,breadth,area;
+		length = 1200;
+		breadth =1400;
+		area= length*breadth;
+		System.out.println("Area of rectangle="+area);
+		}
+}
