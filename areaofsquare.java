0c3251d080d48db088498c990c04c323783043d4
Jayabharath1607 <116068745+Jayabharath1607@users.noreply.github.com>
2022-10-19 13:55:33 +0530

    Update areaofsquare.java

--- areaofsquare.java
+++ areaofsquare.java
@@ -2,8 +2,8 @@
 public class areaofsquare {
 	public static void main(String[] args) {
 		float length, height,area;
-		length=67.3f;
-		height=12.3f;
+		length=30.3f;
+		height=10.7f;
 		
 		area=length*height;
 		System.out.println("Area of reactangle of length="+length+" height="+height +" is "+area);
